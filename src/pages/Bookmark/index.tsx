67090ba5676adf34b116bc6ffdc9acf004dde8a3
saanglee <sangllee20@gmail.com>
2022-09-11 13:37:04 +0900

    Feat : get movies in bookmark

--- src/pages/Bookmark/index.tsx
+++ src/pages/Bookmark/index.tsx
@@ -1,8 +1,26 @@
 import { Movie } from '../../types/type';
 import Card from '../../components/Movie/MovieCard';
+import useGetMovies from '../../hooks/queries/useGetMovies';
+
+const likes = (movies: Movie[]) =>
+  movies.filter((movie) => movie.like === true);
 
 const Bookmark = () => {
-  return <div>Bookmark</div>;
+  const { data: movies } = useGetMovies(likes);
+
+  console.log('moviesBookmarked', movies);
+
+  return (
+    <div>
+      {movies?.map((movie: Movie) => {
+        return (
+          <div key={movie.id}>
+            <Card movie={movie} />
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Bookmark;
