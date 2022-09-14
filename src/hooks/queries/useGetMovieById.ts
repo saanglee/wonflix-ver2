b8fcabcea0fbcc21d97d4a943dde780de6ed79a2
saanglee <sangllee20@gmail.com>
2022-09-14 14:04:09 +0900

    Feat : queryKey

--- src/hooks/queries/useGetMovieById.ts
+++ src/hooks/queries/useGetMovieById.ts
@@ -1,12 +1,15 @@
 import { useQuery, UseQueryOptions } from 'react-query';
 import MovieAPI from '../../api/MovieAPI';
-import { Movie } from '../../types/type';
+import { Movie } from '../../@types/type';
+import queryKeys from '../../react-query/queryKey';
 
 const useGetMovieById = (
   id: number,
   options?: UseQueryOptions<{ data: Movie }, Error>
 ) => {
-  return useQuery<{ data: Movie }>(['movie', id], () => MovieAPI.getById(id));
+  return useQuery<{ data: Movie }>([queryKeys.movies, id], () =>
+    MovieAPI.getById(id)
+  );
 };
 
 useGetMovieById.getKey = (id: number) => ['movie', id];
--- src/react-query/queryKey.ts
+++ src/react-query/queryKey.ts
@@ -2,6 +2,7 @@ const queryKeys = {
   movies: 'movies',
   movie: 'movie',
   bookmark: 'bookmark',
+  list: 'list',
 };
 
 export default queryKeys;
