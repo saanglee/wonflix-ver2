0eb770f6da1c5de950a498b794509f8335566b50
saanglee <sangllee20@gmail.com>
2022-09-14 14:36:36 +0900

    WIP : get movies with like is true

--- src/pages/Bookmark/index.tsx
+++ src/pages/Bookmark/index.tsx
@@ -2,22 +2,32 @@ import { Movie } from '../../@types/type';
 import Card from '../../components/Movie/MovieCard';
 import useGetMovies from '../../hooks/queries/useGetMovies';
 
-const likes = (movies: Movie[]) =>
-  movies.filter((movie) => movie.like === true);
+// const likes = (movies: any) => {
+//   console.log(movies);
+//   return movies.filter((movie: any) => movie.like === true);
+// };
+
+const likes = (movies: any) => {
+  return movies.pages.map((page: any) => {
+    return page.data.filter((movie: Movie) => movie.like === true);
+  });
+};
 
 const Bookmark = () => {
-  const { data: movies } = useGetMovies(likes);
+  const { data } = useGetMovies(likes);
 
-  console.log('moviesBookmarked', movies);
+  // console.log('movies', movies);
 
   return (
     <div>
-      {/* {movies?.map((movie: Movie) => {
-        return (
-          <div key={movie.id}>
-            <Card movie={movie} />
-          </div>
-        );
+      {/* {movies?.pages.map((page) => {
+        return page.data.map((movie: Movie) => {
+          return (
+            <div key={movie.id}>
+              <Card movie={movie} />
+            </div>
+          );
+        });
       })} */}
     </div>
   );
