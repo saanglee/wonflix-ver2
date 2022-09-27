661fd76daf661fe3e9189a0b933e7aad8f965d53
saanglee <sangllee20@gmail.com>
2022-09-27 19:15:17 +0900

    WIP : MovieList, InfinityScroll inlien-style 적용

--- src/components/Movie/MovieList/index.tsx
+++ src/components/Movie/MovieList/index.tsx
@@ -1,22 +1,57 @@
-import Card from '../MovieCard';
-import useGetMovies from '../../../hooks/queries/useGetMovies';
+import { InfiniteData, InfiniteQueryObserverResult } from 'react-query';
 import { Movie } from '../../../@types/type';
-import InfinteScroll from 'react-infinite-scroller';
-import { useInfiniteQuery } from 'react-query';
+import Card from '../MovieCard';
+import InfiniteScroll from 'react-infinite-scroller';
+import styles from './movieList.module.scss';
 
-const MovieList = () => {
-  const { data: movies } = useGetMovies();
+interface MovieListProps {
+  data: InfiniteData<any> | undefined;
+  isLoading: boolean;
+  hasNextPage: boolean | undefined;
+  // fetchNextPage: () => Promise<InfiniteQueryObserverResult>;
+  fetchNextPage: () => void;
+}
+
+// TODO: 나중에 쓸 거
+// const searchResults = (movies: Movie[]) =>
+//   movies.filter((movie) => movie.title.includes('Doctor'));
+
+const MovieList = ({
+  data,
+  isLoading,
+  hasNextPage,
+  fetchNextPage,
+}: MovieListProps) => {
+  if (isLoading) return <div className={styles.loading}>Loading...</div>;
+
+  const loadMore = () => {
+    if (hasNextPage) {
+      fetchNextPage();
+    }
+  };
 
   return (
-    <div>
-      {/* {movies?.map((movie: Movie) => {
-        return (
-          <div key={movie.id}>
-            <Card movie={movie} />
-          </div>
-        );
-      })} */}
-    </div>
+    <InfiniteScroll
+      pageStart={1}
+      loadMore={loadMore}
+      hasMore={hasNextPage}
+      style={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        maxWidth: '1500px',
+        width: '100%',
+      }}
+    >
+      {data?.pages.map((page) => {
+        return page.data.map((movie: Movie) => {
+          return (
+            <div key={movie.id}>
+              <Card movie={movie} />
+            </div>
+          );
+        });
+      })}
+    </InfiniteScroll>
   );
 };
 
